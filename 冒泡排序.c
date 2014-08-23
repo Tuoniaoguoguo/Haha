68e1a23e86487f351c6599e94bb9dda9eb6bf4b4
Tuoniaoguoguo <sphinz@gmail.com>
2014-08-23 11:39:34 +0800

    Create 冒泡排序.c

---
+++ 冒泡排序.c
@@ -0,0 +1,12 @@
+					for(i=0;i<DUTYCYCLE_ARRAY_MAX_I;i++) //Bubble Sort
+					{
+						for(j=0;j<DUTYCYCLE_ARRAY_MAX_I-i;j++)
+						{
+							if(obj->DutyCycle_Array[j+1]<obj->DutyCycle_Array[j])
+							{
+								   tmp=obj->DutyCycle_Array[j+1];
+								   obj->DutyCycle_Array[j+1]=obj->DutyCycle_Array[j];
+								   obj->DutyCycle_Array[j]=tmp;
+							}
+						}
+					}
